b38ebaa2bc023bef6bed47469d0b588a07f1f095
LyricTian <tiannianshou@gmail.com>
2019-10-10 11:10:47 +0800

    更新导入

--- cmd/dataimport/exec.sh
+++ cmd/dataimport/exec.sh
@@ -1,2 +1,2 @@
 go build -o dataimport
-./dataimport -c ./configs/config.toml -d ./configs/excel/济南齐鲁软件园发展中心有限公司.xlsx -dc ./configs/json/济南齐鲁软件园发展中心有限公司_写字楼.json
+./dataimport -c ./configs/config.toml -d ./configs/excel/济南综合保税区开发投资集团有限公司.xlsx -dc ./configs/json/济南综合保税区开发投资集团有限公司_商铺.json
