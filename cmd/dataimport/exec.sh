035f79711d190cd1a19d70fb5edea35118d7397d
LyricTian <tiannianshou@gmail.com>
2019-10-10 10:27:38 +0800

    更新导入

--- cmd/dataimport/exec.sh
+++ cmd/dataimport/exec.sh
@@ -1,2 +1,2 @@
 go build -o dataimport
-./dataimport -c ./configs/config.toml -d ./configs/excel/济南东信开发建设有限公司.xlsx -dc ./configs/json/济南东信开发建设有限公司_厂房.json
+./dataimport -c ./configs/config.toml -d ./configs/excel/资产数据智慧谷2.xlsx -dc ./configs/json/济南高新智慧谷投资置业有限公司_商铺.json
