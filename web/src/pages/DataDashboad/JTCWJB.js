b5b561e72972e8041accc34fed9224aa3ace0b5a
wangjingru <1310238398@qq.com>
2019-10-10 13:32:06 +0800

    修改

--- web/src/pages/DataDashboad/JTCWJB.js
+++ web/src/pages/DataDashboad/JTCWJB.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Chart, Geom, Axis, Tooltip, Coord, Label, Legend } from 'bizcharts';
+import { Chart, Geom, Axis, Tooltip, Coord, Label, Legend, Guide } from 'bizcharts';
 import DataSet from '@antv/data-set';
 
-import { queryClasstify } from '@/services/dataDashboad';
+import { queryClasstify, queryOverview } from '@/services/dataDashboad';
 
 import { formatNumber } from '@/utils/utils';
 
 class JTCWJB extends React.Component {
   state = {
     data: [],
+    annual_actual_income: 0,
   };
 
   componentDidMount() {
-    
     const { params } = this.props;
     this.fetchData(params);
   }
@@ -22,7 +22,7 @@ class JTCWJB extends React.Component {
     if (params.year !== prevProps.params.year) {
       this.fetchData(params);
     }
-    if(params.org_id&&params.org_id !== prevProps.params.org_id){
+    if (params.org_id && params.org_id !== prevProps.params.org_id) {
       this.fetchData(params);
     }
   }
@@ -37,12 +37,19 @@ class JTCWJB extends React.Component {
       }
       this.setState({ data: result });
     });
+    queryOverview(params).then(data => {
+      this.setState({
+        annual_actual_income: formatNumber(data.annual_actual_income, 100 * 10000, 2),
+        annual_plan_income: formatNumber(data.annual_plan_income, 100 * 10000, 2),
+      });
+    });
   };
 
   render() {
     const { height } = this.props;
-    const { data } = this.state;
+    const { data, annual_actual_income } = this.state;
     const { DataView } = DataSet;
+    const { Html } = Guide;
     const dv = new DataView();
     dv.source(data).transform({
       type: 'percent',
@@ -58,14 +65,22 @@ class JTCWJB extends React.Component {
       },
     };
     return (
-      <Chart height={height} data={dv} scale={cols} padding={[30, 50, 50, 50]} forceFit>
-        <Coord type="theta" radius={0.75} />
+      <Chart height={height} data={dv} scale={cols} padding={[30, 80, 50, 50]} forceFit>
+        <Coord type="theta" radius={0.75} innerRadius={0.6} />
         <Axis name="percent" />
         <Legend position="bottom" offsetY={-5} />
         <Tooltip
           showTitle={false}
           itemTpl='<li><span style="background-color:{color};" class="g2-tooltip-marker"></span>{name}: {value}</li>'
         />
+        <Guide>
+          <Html
+            position={['50%', '50%']}
+            html={`<div style="color:#FFFFFF;font-size:10px;text-align: center;width: 10em;">实际收入<br><span style="color:#FFFFFF;font-size:10px">${annual_actual_income}</span><span style="color:#FFFFFF">万元</span></div>`}
+            alignX="middle"
+            alignY="middle"
+          />
+        </Guide>
         <Geom
           type="intervalStack"
           position="percent"
@@ -76,7 +91,7 @@ class JTCWJB extends React.Component {
           tooltip={[
             'item*count',
             (item, count) => {
-              const v = `${(formatNumber(count, 100*10000, 0, 2))}万元`;
+              const v = `${formatNumber(count, 100 * 10000, 0, 2)}万元`;
               return {
                 name: item,
                 value: v,
