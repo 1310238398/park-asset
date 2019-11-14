577dd72bcecd3581b7b95c21a52ed3f8b73ae931
cy <chenyang@antlinker.com>
2019-11-14 20:46:03 +0800

    ...

--- web/src/pages/ProjectManage/ProjectManageCardNew.js
+++ web/src/pages/ProjectManage/ProjectManageCardNew.js
@@ -5,6 +5,7 @@ import PicturesWall from '../../components/PicturesWall/PicturesWall';
 import DicSelect from '@/components/DictionaryNew/DicSelect';
 import Step1 from './Step1';
 import Step2 from './Step2';
+import Step3 from './Step3';
 //import Step3 from './Step3';
 // import GetLocation from './GetLocation';
 
@@ -32,7 +33,7 @@ class ProjectManageCardNew extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      current: 0,
+      current: 2,
     };
   }
 
@@ -41,37 +42,50 @@ class ProjectManageCardNew extends PureComponent {
       type: 'projectManage/queryCompany',
     });
   }
+  // execCallback = () => {
+  //   const { chg } = this.state;
+  //   const {
+  //     projectManage: {  newFormVisible },
 
-  next() {
-    const current = this.state.current + 1;
-    this.setState({ current });
-  }
+  //   } = this.props;
+  //  // this.props.callback(chg && 'ok');
+  //   this.setState({ newFormVisible: false });
+  // };
 
-  prev() {
-    const current = this.state.current - 1;
-    this.setState({ current });
-  }
+  // next() {
+  //   const current = this.state.current + 1;
+  //   this.setState({ current });
+  // }
+
+  // prev() {
+  //   const current = this.state.current - 1;
+  //   this.setState({ current });
+  // }
 
   onOKClick = () => {
-
-    console.log("哈哈哈1")
+    console.log('哈哈哈1');
     const { form, onSubmit } = this.props;
 
-      setTimeout(() => {
-        const { current } = this.state;
-        if (current === 0) {
-          this.step1();
-        }
-        if (current === 1) {
-          this.step2();
-        }
-        if (current === 2) {
-          this.step3();
-        }
-      }, 1);
-  
+    setTimeout(() => {
+      const { current } = this.state;
+      if (current === 0) {
+        this.step1();
+      }
+      if (current === 1) {
+        this.step2();
+      }
+      if (current === 2) {
+        this.step3();
+      }
+    }, 1);
+  };
 
-   
+  onCancelClick = () => {
+    console.log('onCancelClick');
+    const { onCancel } = this.props;
+    this.setState({ current: 2 });
+
+    onCancel();
   };
 
   dispatch = action => {
@@ -80,33 +94,35 @@ class ProjectManageCardNew extends PureComponent {
   };
 
   showStep = step => {
-    console.log("showStep");
-    const cobj = { step, formVisible: step !== -1 };
-   // const columnid = this.getColumnId();
-    // if (!columnid) {
-    //   cobj.step = 0;
-    // }
-    const current = step;
+    console.log('showStep');
+
+    const current = step !== -1 ? step : 0;
     this.setState({ current });
 
-    this.setState({ ...cobj });
+    if (step === -1) {
+      this.handleDataNewFormCancel();
+    }
+    //
+  };
+  handleDataNewFormCancel = () => {
+    this.dispatch({
+      type: 'projectManage/changeNewFormVisible',
+      payload: false,
+    });
   };
   idHandler = id => {
-    console.log("idHandler");
+    console.log('idHandler');
     this.setState({ id });
   };
 
-
-
   setStep1Submit = handler => {
-    console.log("setStep1Submit");
+    console.log('setStep1Submit');
     this.step1 = handler;
   };
 
   step1Next = () => {
-    console.log("step1Next");
+    console.log('step1Next');
     this.showStep(1);
-    this.setState({ chg: true });
   };
 
   step2Next = () => {
@@ -114,8 +130,9 @@ class ProjectManageCardNew extends PureComponent {
   };
 
   step3Next = () => {
+    console.log('step3Next');
     this.showStep(-1);
-    this.execCallback();
+    // this.execCallback();
   };
 
   setStep2Submit = handler => {
@@ -158,45 +175,48 @@ class ProjectManageCardNew extends PureComponent {
           </Row>
         }
         width="60%"
-     
+        centered={true}
         visible={newFormVisible}
         maskClosable={false}
         confirmLoading={submitting}
-        destroyOnClose
+        destroyOnClose={true}
         onOk={this.onOKClick}
-        onCancel={onCancel}
-        style={{ top: 20 }}
-        bodyStyle={{ maxHeight: 'calc( 100vh - 158px )', overflowY: 'auto' }}
+        onCancel={this.onCancelClick}
+        //   style={{ height: 800 }}
+        bodyStyle={{ maxHeight: 'calc( 100vh - 158px )', height: '500px', overflowY: 'auto' }}
       >
         <div>
           {/* <div className="steps-content">{steps[current].content}</div> */}
           <div className="steps-action">
-            {current  ==0 && (
-             <Step1
-             //id={columnid}
-             callback={this.setStep1Submit}
-             idHandler={this.idHandler}
-             nextHandler={this.step1Next}
-           //  org={orgid}
-           />
+            {current == 0 && (
+              <Step1
+                //id={columnid}
+                callback={this.setStep1Submit}
+                idHandler={this.idHandler}
+                nextHandler={this.step1Next}
+                //  org={orgid}
+              />
             )}
             {current === 1 && (
-             <Step2
-             //id={columnid}
-             callback={this.setStep2Submit}
-             idHandler={this.idHandler}
-             nextHandler={this.step2Next}
-           //  org={orgid}
-           />
+              <Step2
+                //id={columnid}
+                callback={this.setStep2Submit}
+                idHandler={this.idHandler}
+                nextHandler={this.step2Next}
+                //  org={orgid}
+              />
             )}
             {current === 2 && (
-              <Button style={{ marginLeft: 8 }} onClick={() => this.prev()}>
-                Previous
-              </Button>
+              <Step3
+                //id={columnid}
+                callback={this.setStep3Submit}
+                idHandler={this.idHandler}
+                nextHandler={this.step3Next}
+                //  org={orgid}
+              />
             )}
           </div>
         </div>
-       
       </Modal>
     );
   }
--- web/src/pages/ProjectManage/Step2.js
+++ web/src/pages/ProjectManage/Step2.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'dva';
-import { Form, Input, Modal, Row, Col, Select, Radio ,message} from 'antd';
+import { Form, Input, Modal, Row, Col, Select, Radio, message, Checkbox } from 'antd';
 import PicturesWall from '../../components/PicturesWall/PicturesWall';
 import DicSelect from '@/components/DictionaryNew/DicSelect';
 
@@ -8,7 +8,7 @@ const FormItem = Form.Item;
 const { Option } = Select;
 
 @connect(state => ({
-  projectManage: state.projectManage,
+  //projectManage: state.projectManage,
 }))
 @Form.create()
 export default class Step2 extends PureComponent {
@@ -18,9 +18,35 @@ export default class Step2 extends PureComponent {
     callback(this.formSubmit);
   }
 
+
+  
+
   state = {
     formData: {},
     treeData: [],
+    yetai : [
+      {
+        code: "a",
+        name: "住宅"
+      },
+      {
+        code: "b",
+        name:"地上车位"
+      },
+      {
+        code:"c",
+        name:"地下车库"
+      },
+      {
+        code:"d",
+        name:"写字楼"
+      },
+      {
+        code:"e",
+        name:"公寓"
+      }
+    ]
+   
   };
 
   componentDidMount() {
@@ -33,99 +59,119 @@ export default class Step2 extends PureComponent {
     const { dispatch } = this.props;
     dispatch(action);
   };
-  
 
   formSubmit = () => {
-    
-      const { form, onSubmit,nextHandler } = this.props;
-  
-      form.validateFieldsAndScroll((err, values) => {
-        if (err) {
+    console.log('Step2');
+    const { form, onSubmit, nextHandler } = this.props;
+
+    form.validateFieldsAndScroll((err, values) => {
+      if (err) {
+
+
+        return;
+      }
+      const formData = { ...values };
+      
+      let hasDataCount = 0;
+      for (let i = 0; i < this.state.yetai.length; i++) {
+
+        let item = this.state.yetai[i];
+        if (formData[item.code]) {
+          hasDataCount++;
+
+        }
+        
+        console.log(formData[item.code]);
+        console.log(formData[item.code+"mj"]);
+       
+        if (i == (this.state.yetai.length - 1) && hasDataCount == 0) {
+          message.warning('请至少选择一个业态');
+
           return;
         }
-        const formData = { ...values };
-        if (formData.photo && formData.photo.length > 0) {
-          formData.photo = formData.photo.join('');
-        } else {
-          formData.photo = '';
-        }
-  
-        if (formData.asset_type && formData.asset_type.length > 0) {
-          formData.asset_type = formData.asset_type.join(',');
-        } else {
-          formData.asset_type = '';
-        }
-       // onSubmit(formData);
-       message.success('保存成功');
-       if (nextHandler) nextHandler();
-      });
-   
+
+      }
+
+     
+    
+      // onSubmit(formData);
+      console.log(form.getFieldsValue());
+      
+
+      console.log(formData);
+      message.success('保存成功');
+      if (nextHandler) nextHandler();
+    });
   };
 
   render() {
     const {
-      projectManage: {  formData, companyList, poltList },
-      form: { getFieldDecorator },
+     // projectManage: { formData, companyList, poltList },
+      form: { getFieldDecorator, getFieldValue },
       onCancel,
     } = this.props;
-
-    const RadioGroup = Radio.Group;
+    const {yetai} = this.state;
+ 
     const formItemLayout = {
       labelCol: {
-        span: 6,
+        xs: { span: 24 },
+        sm: { span: 4 },
       },
       wrapperCol: {
-        span: 18,
+        xs: { span: 24 },
+        sm: { span: 20 },
       },
     };
+
+  
     return (
       <Form>
-    
-    
-     
-      <Row>
-        <Col span={12}>
-          <Form.Item {...formItemLayout} label="资产性质">
-            {getFieldDecorator('nature', {
-            //  initialValue: formData.nature,
-              rules: [
-                {
-                  required: false,
-                  message: '请选择',
-                },
-              ],
-            })(
-              <DicSelect
-                vmode="int"
-                pcode="pa$#anature"
-                selectProps={{ placeholder: '请选择' }}
-              />
-            )}
-          </Form.Item>
-        </Col>
 
-        <Col span={12}>
-          <Form.Item {...formItemLayout} label="资产类型">
-            {getFieldDecorator('asset_type', {
-             // initialValue: formData.asset_type,
-              rules: [
-                {
-                  required: true,
-                  message: '请选择资产类型',
-                },
-              ],
-            })(
-              <DicSelect
-                vmode="sting"
-                pcode="pa$#atype"
-                selectProps={{ mode: 'multiple', placeholder: '请选择' }}
-              />
-            )}
-          </Form.Item>
-        </Col>
-      </Row>
-     
-    </Form>
+        <Row>
+          <Col span={4} style={{textAlign:"right"}}><span style={{lineHeight:"40px"}}>选择相关业态：&nbsp;&nbsp;&nbsp;</span></Col>
+          <Col span={20}>
+          {
+                yetai.map((item, index) => 
+                <Row
+                style={{
+                  height: '59px',
+                  display: 'flex',
+                  justifyContent: 'flex-start',
+                  alignItems: 'center',
+                }}
+              >
+                <Col span={4}>
+                <Form.Item {...formItemLayout} label="" >
+                {getFieldDecorator(item.code, {
+                 // initialValue: item.name,
+                
+                
+                })( <Checkbox value={item.name} >{item.name}</Checkbox>)}
+                  </Form.Item>
+                  
+                </Col>
+                <Col span={12}>
+                <Form.Item {...formItemLayout} label="建筑面积">
+                {getFieldDecorator(item.code+"mj", {
+                 // initialValue: formData.address,
+                  rules: [
+                    {
+                      required: getFieldValue(item.code),
+                      message: '请输入建筑面积',
+                    },
+                  ],
+                })(<Input  disabled={!getFieldValue(item.code)} placeholder="请输入建筑面积" />)}
+              </Form.Item>
+                </Col>
+              </Row>)
+              
+              }
+
+          </Col>
+        </Row>
+      
+      
+      </Form>
     );
   }
 }
