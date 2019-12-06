68b65e4ff626bc353e07c8d67742f63b3b402e07
dengguangji <dengguangji@antlinker.com>
2019-12-06 14:15:57 +0800

    收益测试

--- internal/app/routers/api/ctl/c_proj_income_calculation.go
+++ internal/app/routers/api/ctl/c_proj_income_calculation.go
@@ -5,6 +5,7 @@ import (
 	"gxt-park-assets/internal/app/errors"
 	"gxt-park-assets/internal/app/ginplus"
 	"gxt-park-assets/internal/app/schema"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -166,6 +167,143 @@ func (a *ProjIncomeCalculation) Update(c *gin.Context) {
 // @Router DELETE /api/v1/proj-income-calculations/{id}
 func (a *ProjIncomeCalculation) Delete(c *gin.Context) {
 	err := a.ProjIncomeCalculationBll.Delete(ginplus.NewContext(c), c.Param("id"))
+	if err != nil {
+		ginplus.ResError(c, err)
+		return
+	}
+	ginplus.ResOK(c)
+}
+
+// CreateVersion 保存新版本
+// @Summary 保存新版本
+// @Param Authorization header string false "Bearer 用户令牌"
+// @Param id path string true "项目ID"
+// @Param body body []*schema.ProjCompareItem true
+// @Success 200 schema.HTTPStatus "{status:OK}"
+// @Failure 400 schema.HTTPError "{error:{code:0,message:无效的请求参数}}"
+// @Failure 401 schema.HTTPError "{error:{code:0,message:未授权}}"
+// @Failure 500 schema.HTTPError "{error:{code:0,message:服务器错误}}"
+// @Router POST /api/v1/proj-version/:id
+func (a *ProjIncomeCalculation) CreateVersion(c *gin.Context) {
+	projectID := c.Param("id")
+
+	data := []*schema.ProjCompareItem{}
+
+	if err := ginplus.ParseJSON(c, &data); err != nil {
+		ginplus.ResError(c, err)
+		return
+	}
+
+	if err := a.ProjIncomeCalculationBll.CreateVersion(ginplus.NewContext(c), projectID, data); err != nil {
+		ginplus.ResError(c, err)
+		return
+	}
+	return
+}
+
+// UpdateVersion 保存旧版本
+// @Summary 保存旧版本
+// @Param Authorization header string false "Bearer 用户令牌"
+// @Param id path string true "项目ID"
+// @Param body body []*schema.ProjCompareItem true
+// @Success 200 schema.HTTPStatus "{status:OK}"
+// @Failure 400 schema.HTTPError "{error:{code:0,message:无效的请求参数}}"
+// @Failure 401 schema.HTTPError "{error:{code:0,message:未授权}}"
+// @Failure 500 schema.HTTPError "{error:{code:0,message:服务器错误}}"
+// @Router PUT /api/v1/proj-version/:id
+func (a *ProjIncomeCalculation) UpdateVersion(c *gin.Context) {
+	projectID := c.Param("id")
+
+	data := []*schema.ProjCompareItem{}
+
+	if err := ginplus.ParseJSON(c, &data); err != nil {
+		ginplus.ResError(c, err)
+		return
+	}
+
+	if err := a.ProjIncomeCalculationBll.UpdateVersion(ginplus.NewContext(c), projectID, data); err != nil {
+		ginplus.ResError(c, err)
+		return
+	}
+	return
+}
+
+// QueryVersionCompare 查询版本对比版本
+// @Summary 查询版本对比版本
+// @Param Authorization header string false "Bearer 用户令牌"
+// @Param id path string true "项目ID"
+// @Param list query string true "版本ID列表,逗号分隔允许current、last、beforeLast"
+// @Success 200 []*schema.ProjCompareItem
+// @Failure 400 schema.HTTPError "{error:{code:0,message:无效的请求参数}}"
+// @Failure 401 schema.HTTPError "{error:{code:0,message:未授权}}"
+// @Failure 500 schema.HTTPError "{error:{code:0,message:服务器错误}}"
+// @Router GET /api/v1/proj-version/:id/compare
+func (a *ProjIncomeCalculation) QueryVersionCompare(c *gin.Context) {
+	projectID := c.Param("id")
+	versions := strings.Split(c.Query("list"), ",")
+
+	result, err := a.ProjIncomeCalculationBll.GetVersionComparison(
+		ginplus.NewContext(c), projectID, versions...)
+	if err != nil {
+		ginplus.ResError(c, err)
+		return
+	}
+	ginplus.ResSuccess(c, result)
+}
+
+// Apply 收益测试审核申请
+// @Summary 收益测试审核申请
+// @Param Authorization header string false "Bearer 用户令牌"
+// @Param id path string true "项目ID"
+// @Success 200 schema.HTTPStatus "{status:OK}"
+// @Failure 400 schema.HTTPError "{error:{code:0,message:无效的请求参数}}"
+// @Failure 401 schema.HTTPError "{error:{code:0,message:未授权}}"
+// @Failure 500 schema.HTTPError "{error:{code:0,message:服务器错误}}"
+// @Router PUT /api/v1/proj-version/:id/apply
+func (a *ProjIncomeCalculation) Apply(c *gin.Context) {
+	projectID := c.Param("id")
+
+	err := a.ProjIncomeCalculationBll.Apply(ginplus.NewContext(c), projectID)
+	if err != nil {
+		ginplus.ResError(c, err)
+		return
+	}
+	ginplus.ResOK(c)
+}
+
+// Pass 通过收益测试审核申请
+// @Summary 通过收益测试审核申请
+// @Param Authorization header string false "Bearer 用户令牌"
+// @Param id path string true "项目ID"
+// @Success 200 schema.HTTPStatus "{status:OK}"
+// @Failure 400 schema.HTTPError "{error:{code:0,message:无效的请求参数}}"
+// @Failure 401 schema.HTTPError "{error:{code:0,message:未授权}}"
+// @Failure 500 schema.HTTPError "{error:{code:0,message:服务器错误}}"
+// @Router PUT /api/v1/proj-version/:id/pass
+func (a *ProjIncomeCalculation) Pass(c *gin.Context) {
+	projectID := c.Param("id")
+
+	err := a.ProjIncomeCalculationBll.Pass(ginplus.NewContext(c), projectID)
+	if err != nil {
+		ginplus.ResError(c, err)
+		return
+	}
+	ginplus.ResOK(c)
+}
+
+// Reject 驳回收益测试审核申请
+// @Summary 驳回收益测试审核申请
+// @Param Authorization header string false "Bearer 用户令牌"
+// @Param id path string true "项目ID"
+// @Success 200 schema.HTTPStatus "{status:OK}"
+// @Failure 400 schema.HTTPError "{error:{code:0,message:无效的请求参数}}"
+// @Failure 401 schema.HTTPError "{error:{code:0,message:未授权}}"
+// @Failure 500 schema.HTTPError "{error:{code:0,message:服务器错误}}"
+// @Router PUT /api/v1/proj-version/:id/reject
+func (a *ProjIncomeCalculation) Reject(c *gin.Context) {
+	projectID := c.Param("id")
+
+	err := a.ProjIncomeCalculationBll.Reject(ginplus.NewContext(c), projectID)
 	if err != nil {
 		ginplus.ResError(c, err)
 		return
