377eecf0f29339e62ba109a38a055605e0c7466c
dengguangji <dengguangji@antlinker.com>
2019-12-25 17:27:02 +0800

    bug修正

--- internal/app/bll/impl/internal/b_proj_dynamic_cost.go
+++ internal/app/bll/impl/internal/b_proj_dynamic_cost.go
@@ -11,8 +11,8 @@ import (
 )
 
 // NewProjDeliveryStandard 创建成本项目交付标准
-func NewProjDynamicCost(mTrans model.ITrans) bll.IProjDeliveryStandard {
-	return &ProjDeliveryStandard{
+func NewProjDynamicCost(mTrans model.ITrans) bll.IProjDynamicCost {
+	return &ProjDynamicCost{
 		Trans: mTrans,
 	}
 }
@@ -23,7 +23,7 @@ type ProjDynamicCost struct {
 }
 
 //TODO QueryCostTree 查询成本科目树结构
-func (a *ProjDynamicCost) QueryCostTree(ctx context.Context, projectID string) ([]*schema.ProjDynamicCostTree, error) {
+func (a *ProjDynamicCost) QueryCostTree(ctx context.Context, projectID string) (schema.ProjDynamicCostTrees, error) {
 	example := []*schema.ProjDynamicCostTree{
 		&schema.ProjDynamicCostTree{
 			CostID: "111",
